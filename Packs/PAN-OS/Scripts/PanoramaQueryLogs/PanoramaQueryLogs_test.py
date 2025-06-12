2d44ab28431585382f73b3d0559eaa3c2d4d1e68
aneeshamore <amore@paloaltonetworks.com>
2025-06-12 12:48:28 -0700

    unit test for PanoramaQueryLogs wrapper script.

--- Packs/PAN-OS/Scripts/PanoramaQueryLogs/PanoramaQueryLogs_test.py
+++ Packs/PAN-OS/Scripts/PanoramaQueryLogs/PanoramaQueryLogs_test.py
@@ -1,38 +1,28 @@
-"""Base Script for Cortex XSOAR - Unit Tests file
+from pytest_mock import MockerFixture
 
-Pytest Unit Tests: all funcion names must start with "test_"
-
-More details: https://xsoar.pan.dev/docs/integrations/unit-testing
-
-MAKE SURE YOU REVIEW/REPLACE ALL THE COMMENTS MARKED AS "TODO"
-
-"""
-
-import json
-
-
-def util_load_json(path):
-    with open(path, encoding="utf-8") as f:
-        return json.loads(f.read())
-
-
-# TODO: REMOVE the following dummy unit test function
-def test_basescript_dummy():
-    """Tests helloworld-say-hello command function.
-
-    Checks the output of the command function with the expected output.
-
-    No mock is needed here because the say_hello_command does not call
-    any external API.
+def test_main(mocker: MockerFixture):
     """
-    from BaseScript import basescript_dummy_command
+    Given:
+        Command args.
+    When:
+        Calling `main`.
+    Assert:
+        Ensure `execute_polling_command` is called once with the correct command name and args.
+    """
+    from PanoramaQueryLogs import main
 
-    args = {"dummy": "this is a dummy response"}
-    response = basescript_dummy_command(args)
-
-    mock_response = util_load_json("test_data/basescript-dummy.json")
-
-    assert response.outputs == mock_response
-
-
-# TODO: ADD HERE your unit tests
+    args = {
+        "log-type": "threat",
+        "query": "source:1.1.1.1",
+        "number_of_logs": "100",
+        "timeout": "120",
+        "interval_in_seconds": "10"
+    }
+    
+    mocker.patch("PanoramaQueryLogs.demisto.args", return_value=args)
+    mock_execute_polling_command = mocker.patch("PanoramaQueryLogs.execute_polling_command", return_value=[])
+    
+    main()
+    
+    assert mock_execute_polling_command.call_count == 1
+    assert mock_execute_polling_command.call_args[0] == ("pan-os-query-logs", args)
